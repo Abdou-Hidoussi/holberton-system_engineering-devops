d007e6dcd67b000f67ca1156d5b43e73efd31c9f
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-12-08 15:45:39 +0000

    initial

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Task 4
-file_line { 'holberton':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
-
-file_line { 'holberton':
+file_line { 'pk':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
