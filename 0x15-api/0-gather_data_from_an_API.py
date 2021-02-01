00ca3517ad054763a44abb9651298f1a2ce835cd
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-02-01 16:15:56 +0000

    initial

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1])
+    name = data.json().get('name')
+
+    data = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1] + '/todos')
+    done = 0
+    done_task = []
+    for task in data.json():
+        if task['completed']:
+            done_task.append(task['title'])
+            done += 1
+    total = len(data.json())
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for task in done_task:
+        print("     {}".format(task))
