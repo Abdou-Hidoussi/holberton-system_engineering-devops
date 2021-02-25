c64d06d5fcf6c6aa0951582bcfdec21fe77c712d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-02-25 19:44:35 +0000

    inital

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Task 0
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}         ->
+exec { 'nginx-restart':
+  command => 'nginx restart',
+}
